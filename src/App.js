076cfeb018297c7641828f9425f889815c3fd58a
Andrew De Torres <andrew.de.torres@hotmail.co.uk>
2020-01-07 21:13:14 +0000

    Fontawesome and rotation buttons added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './assets/sass/vendor/main.scss';
+
+
+import { library }  from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 
 // Import components
 import GradientSelector from './components/GradientSelector/GradientSelector';
 
-function App() {
-  return (
-    <div className="App">
-      <GradientSelector />
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    library.add(fas, fab, far);
+
+    return (
+      <div className="App">
+        <GradientSelector />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/GradientSelector/GradientSelector.jsx
+++ src/components/GradientSelector/GradientSelector.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { SketchPicker } from 'react-color';
 import { CopyBlock } from 'react-code-blocks'
 import atomOneLight from 'react-code-blocks/build/cjs/themes/atom-one-light';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class GradientSelector extends Component {
   state = {
@@ -91,33 +92,66 @@ class GradientSelector extends Component {
     };
 
     const code = `
-      background: linear-gradient(rgba(${this.state.firstBackground.r},${this.state.firstBackground.g},${this.state.firstBackground.b},${this.state.firstBackground.a}),rgba(${this.state.secondBackground.r},${this.state.secondBackground.g},${this.state.secondBackground.b},${this.state.secondBackground.a}))
+    background: rgba(${this.state.firstBackground.r},${this.state.firstBackground.g},${this.state.firstBackground.b},${this.state.firstBackground.a});
+    background: -webkit-linear-gradient(rgba(${this.state.firstBackground.r},${this.state.firstBackground.g},${this.state.firstBackground.b},${this.state.firstBackground.a}),rgba(${this.state.secondBackground.r},${this.state.secondBackground.g},${this.state.secondBackground.b},${this.state.secondBackground.a}));
+    background: linear-gradient(rgba(${this.state.firstBackground.r},${this.state.firstBackground.g},${this.state.firstBackground.b},${this.state.firstBackground.a}),rgba(${this.state.secondBackground.r},${this.state.secondBackground.g},${this.state.secondBackground.b},${this.state.secondBackground.a}));
     `;
 
     return (
       <div className="main-container d-flex justify-content-center align-items-center" style={backgroundStyle}>
-        <div className="card w-50">
+        <div className="card w-50 border-0 shadow">
           <div className="card-body">
             <div className="row my-3">
-              <div className="col-md-6 col-12 text-center">
+              <div className="col w-auto text-center my-3">
+                <h1>Quick Gradient</h1>
               </div>
             </div>
             <div className="row justify-content-center my-3">
-              <div className="col-2 text-center">
+              <div className="col w-auto text-center">
                 <button onClick={ this.handleClick } className="color-button border-0" style={firstButtonStyle}></button>
                 {firstPicker}
+                <p>1st</p>
               </div>
-              <div className="col-2 text-center">
+              <div className="col w-auto text-center">
                 <button onClick={ this.handleSecondClick } className="color-button border-0" style={secondButtonStyle}></button>
                 {secondPicker}
+                <p>2nd</p>
               </div>
             </div>
-            <div className="row justify-content-center my-3">
+
+            <div className="row justify-content-around my-3">
+              <div className="grad grad-top">
+                <FontAwesomeIcon icon={['far', 'arrow-alt-circle-up']} className="my-auto"/>
+              </div>
+              <div className="grad grad-top-right">
+                <FontAwesomeIcon icon={['far', 'arrow-alt-circle-up']} className="my-auto"/>
+              </div>
+              <div className="grad grad-right">
+                <FontAwesomeIcon icon={['far', 'arrow-alt-circle-up']} className="my-auto"/>
+              </div>
+              <div className="grad grad-bottom-right">
+                <FontAwesomeIcon icon={['far', 'arrow-alt-circle-up']} className="my-auto"/>
+              </div>
+              <div className="grad grad-bottom">
+                <FontAwesomeIcon icon={['far', 'arrow-alt-circle-up']} className="my-auto"/>
+              </div>
+              <div className="grad grad-bottom-left">
+                <FontAwesomeIcon icon={['far', 'arrow-alt-circle-up']} className="my-auto"/>
+              </div>
+              <div className="grad grad-left">
+                <FontAwesomeIcon icon={['far', 'arrow-alt-circle-up']} className="my-auto"/>
+              </div>
+              <div className="grad grad-top-left">
+                <FontAwesomeIcon icon={['far', 'arrow-alt-circle-up']} className="my-auto"/>
+              </div>
+            </div>
+            <div className="row justify-content-center my-3 mx-auto">
               <CopyBlock
                 text={code}
                 language="text"
                 theme={atomOneLight}
                 showLineNumbers={false}
+                wrapLines
                 />
             </div>
           </div>
