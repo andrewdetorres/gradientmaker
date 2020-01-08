c4965bc016a3f9f38c0594ea4ff046256125c48e
Andrew de torres <andrew.de.torres@hotmail.co.uk>
2020-01-08 16:29:35 +0000

    Responsive issues fixed

--- src/components/Buttons/ContributeButton.jsx
+++ src/components/Buttons/ContributeButton.jsx
@@ -5,7 +5,7 @@ class ContributeButton extends Component {
   render() {
     return (
       <a href="https://github.com/andrewdetorres/quickgradient">
-        <button className="btn btn-github text-white py-1">
+        <button className="btn btn-github text-white py-1 my-1">
           <FontAwesomeIcon icon={['fab', 'github']} className="mr-2 my-auto"/>
           Contribute
         </button>
--- src/components/Buttons/SupportButton.jsx
+++ src/components/Buttons/SupportButton.jsx
@@ -4,14 +4,14 @@ class SupportButton extends Component {
   render() {
     return (
       <a href="https://www.buymeacoffee.com/8jtaCdy">
-        <button className="btn btn-coffee text-white py-1 mx-3">
+        <button className="btn btn-coffee text-white py-1 my-1 mx-3">
           <img
             src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
             alt="Buy me a coffee"
             class="mr-2 my-auto"
             style={{"height":"22px"}}
           />
-          Buy me a coffee
+          Buy a coffee
         </button>
       </a>
     )
